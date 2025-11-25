3ecbd690a35290e2a09d766490f50f4bf7334c5d
a855250a <a855250a@gmail.com>
2025-11-25 23:07:13 +0800

    initial frontend

---
+++ cat/register.js
@@ -0,0 +1,18 @@
+console.log("register.js loaded");
+
+document.getElementById("registerForm").addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    const res = await fetch("http://localhost:3000/register", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ name, email, password })
+    });
+
+    const data = await res.json();
+    document.getElementById("msg").innerText = data.message;
+});
